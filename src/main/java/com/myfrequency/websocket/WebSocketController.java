0a99de1b1c5772a05b53727a86d60616b671dcdd
miccu64 <98konrad98@gmail.com>
2021-05-18 12:27:02 +0200

    Still cant send msg

--- src/main/java/com/myfrequency/websocket/WebSocketController.java
+++ src/main/java/com/myfrequency/websocket/WebSocketController.java
@@ -6,6 +6,7 @@ import com.myfrequency.models.TransportSingleton;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class WebSocketController {
@@ -18,5 +19,21 @@ public class WebSocketController {
         TransportSingleton.getInstance().model = message;
         return new ResultModel(100.F,false);
     }
+
+    @MessageMapping("/hello")
+    @SendTo("/freqPlay/connected")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+/*
+    @MessageMapping("/sendData")
+    @SendTo("/freqPlay/connected")
+    public ResultModel result(String message) {
+        //update values in transport singleton
+        System.out.println(message);
+        //TransportSingleton.getInstance().model = message;
+        return new ResultModel(100.F,false);
+    }*/
 
 }
